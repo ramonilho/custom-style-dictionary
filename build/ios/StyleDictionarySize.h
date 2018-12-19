a2fd9ff084665eb97625f409650273d3f5ebaab9
Ramon Honorio <ramonaqh@gmail.com>
2018-12-19 15:08:04 -0200

    initial commit

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Wed, 19 Dec 2018 17:07:52 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Wed, 19 Dec 2018 17:07:52 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
